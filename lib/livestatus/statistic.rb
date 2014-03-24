3018096c64bda0a0954d30fb2211982444fe3058
Matt Delves <matt@reformedsoftware.com>
2014-03-24 19:16:02 +0900

    [UPDATE] setup initialize method for statistic

--- lib/livestatus/statistic.rb
+++ lib/livestatus/statistic.rb
@@ -1,4 +1,29 @@
 module Livestatus
   class Statistic
+
+    attr_accessor :timestamp,
+                  :controller,
+                  :view,
+                  :view_runtime,
+                  :db_runtime,
+                  :format,
+                  :method,
+                  :path,
+                  :params
+
+    def initialize options
+      @timestamp = Time.now
+      @controller = options[:controller]
+      @view = options[:view]
+      @view_runtime = options[:view_runtime]
+      @db_runtime = options[:db_runtime]
+      @format = options[:format]
+      @method = options[:method]
+      @path   = options[:path]
+      @params = options[:params]
+    end
+
+
+
   end
 end
