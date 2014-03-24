b7a9a53715a491cde2c9465dc82dc314b35b5780
Matt Delves <matt@reformedsoftware.com>
2014-03-24 19:17:03 +0900

    [UPDATE] test for creating statistic classes

--- spec/lib/livestatus/statistic_spec.rb
+++ spec/lib/livestatus/statistic_spec.rb
@@ -3,6 +3,27 @@ require 'livestatus/statistic'
 
 describe Livestatus::Statistic do
 
-  pending "is awesome"
+  before :each do
+#    REDIS.flush
+  end
+
+  after :each do
+
+  end
+
+  it "formats from notifications hash" do
+    notifications = {
+                     controller: "FoobarController",
+                     action: "home",
+                     params: {"controller" => "foobar", "action" => "home"},
+                     format: :html,
+                     path: "/",
+                     status: 200,
+                     view_runtime: 220.13,
+                     db_runtime: 12.0
+                    }
+    statistic = Livestatus::Statistic.new notifications
+    expect(statistic.controller).to eql "FoobarController"
+  end
 
 end
