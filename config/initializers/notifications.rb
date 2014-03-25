ad04c3a95347a8453b4e34a61a7a707cea66295f
Matt Delves <matt@reformedsoftware.com>
2014-03-25 15:15:19 +0900

    [UPDATE] notification now saves statistic when generated

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -1,5 +1,4 @@
 ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |name, start, finish, id, payload|
-
-  # Payload: {:controller=>"DashboardController", :action=>"home", :params=>{"controller"=>"dashboard", "action"=>"home"}, :format=>:html, :method=>"GET", :path=>"/", :status=>200, :view_runtime=>220.13, :db_runtime=>0}
-
+  # Save the payload
+  Statistic.save_notification payload
 end
