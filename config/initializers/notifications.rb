709adfc1ad60bd02ac857eeded4c22b01030615b
Matt Delves <matt@reformedsoftware.com>
2014-03-25 16:59:41 +0900

    [UPDATE] save notifications to redis

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -1,4 +1,6 @@
+require 'livestatus/statistic'
+
 ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |name, start, finish, id, payload|
   # Save the payload
-  Statistic.save_notification payload
+  Livestatus::Statistic.save_notification payload
 end
