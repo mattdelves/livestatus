3e2a2fb3b4ace6f980791653c3c88847774d719c
Matt Delves <matt@reformedsoftware.com>
2014-05-12 00:16:03 +1000

    [UPDATE] make sure that graphs are displayed rather than lists of points

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -2,5 +2,6 @@ require 'livestatus/statistic'
 
 ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |name, start, finish, id, payload|
   # Save the payload
+  return if payload[:controller] == "Livestatus::DashboardController" && payload[:action] == "stream"
   Livestatus::Statistic.save_notification payload
 end
