12093fa0072ec5204e0c3ea78af79ab02865289e
Matthew Delves <matt@reformedsoftware.com>
2014-06-21 12:13:23 +1000

    better handling of when to send notification

--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -2,6 +2,7 @@ require 'livestatus/statistic'
 
 ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |name, start, finish, id, payload|
   # Save the payload
-  return if payload[:controller] == "Livestatus::DashboardController" && payload[:action] == "stream"
-  Livestatus::Statistic.save_notification payload
+  unless payload[:controller] == "Livestatus::DashboardController" && payload[:action] == "stream"
+    Livestatus::Statistic.save_notification payload
+  end
 end
