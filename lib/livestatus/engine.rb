336fec3c3dd26e29ffe250e05704b7459529c71c
Matthew Delves <matt@reformedsoftware.com>
2014-06-21 12:13:43 +1000

    make sure assets get precompiled

--- lib/livestatus/engine.rb
+++ lib/livestatus/engine.rb
@@ -1,5 +1,11 @@
 module Livestatus
   class Engine < ::Rails::Engine
     isolate_namespace Livestatus
+
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w{ rickshaw.js d3.v2.js }
+      Rails.application.config.assets.precompile += %w{ rickshaw.css }
+
+    end
   end
 end
